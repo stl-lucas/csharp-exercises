5f95bef538b310120c9d465817ef4e3f73dc729a
Tim Lucas <tllucas1s@gmail.com>
2018-05-09 09:29:11 -0500

    Completed Area studio exercise.

--- Area/Program.cs
+++ Area/Program.cs
@@ -10,11 +10,17 @@ namespace Area
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a radius: ");
+            double r, a, pi;
+
+            pi = 3.14;
+
+            Console.Write("Enter a radius: ");
+            r = double.Parse(Console.ReadLine());
+
+            a = pi * r * r;
+
+            Console.WriteLine("The area of a circle with radius " + r + " is: " + a);
             Console.ReadLine();
-
-
-
         }
     }
 }
